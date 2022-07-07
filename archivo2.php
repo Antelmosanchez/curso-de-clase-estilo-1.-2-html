eac9bd311fa8b37750e7b11aaaa4122481452368
antelmo <shilonantelmo@gimail.com>
2022-07-06 23:32:30 -0500

    unidad lll.tarea 2

---
+++ archivo2.php
@@ -0,0 +1,54 @@
+<html>
+  <body>
+    <hr>
+    <div style="background: red; width: 300px; color:black">
+    <?php
+  
+      $num1=10;
+      $num2=20;
+     
+     echo "la suma de ".$num1." + ".$num2." es  igual a " .$num1 + $num2;
+
+    ?>
+   </div>
+   <hr>
+    <div style="background: red; width: 300px; color:black">
+    <?php
+  
+      $num1=number_format(3.45,2);
+      $num2=number_format(4.34,2);
+     
+     echo "la suma de ".$num1." + ".$num2." es  igual a " .$num1 + $num2;
+
+    ?>
+   </div>
+   </div>
+   <hr>
+    <div style="background: red; width: 300px; color:black">
+    <?php
+  
+      $num1=10;
+      $num2=20;
+     
+     echo "la multiplicacion de ".$num1." * ".$num2." es  igual a " .$num1 * $num2;
+
+    ?>
+   </div>
+   </div>
+   <hr>
+    <div style="background: red; width: 300px; color:black">
+    <?php
+  
+      $texto1 = "antelmo"; 
+      $texto2 = " shilon";
+     
+     echo $texto1.$texto2;
+
+    ?>
+   </div>
+ 
+  
+
+   </div>
+  </body>  
+</html>  
