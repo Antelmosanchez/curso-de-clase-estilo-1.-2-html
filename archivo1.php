01a882596f20ba510255f2e4f2fe56f736cf1a41
antelmo <shilonantelmo@gimail.com>
2022-07-06 23:30:30 -0500

    unidad lll.tarea 1

---
+++ archivo1.php
@@ -0,0 +1,19 @@
+
+<html>
+
+<body>
+
+<div style="background: red; width: 300px; color:black">
+<?php
+
+
+
+   echo "Nombre : Antelmo <br>";
+
+   echo "Cuatrimestre : 3a";
+
+?>
+</div>
+</body>
+
+</html>
